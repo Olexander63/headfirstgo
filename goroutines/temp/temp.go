1dcea5cf36eefe7c93a8b3ec7a366015ed12da1e
Oleksandr Omelchenko <olexander63@gmail.com>
2022-08-31 18:00:59 +0300

    add channel, greeting and update temp

--- goroutines/temp/temp.go
+++ goroutines/temp/temp.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	ioutil "io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Page struct {
+	URL  string
+	Size int
+}
+
 func main() {
-	responseSize("https://example.com/")
-	responseSize("https://golang.org/")
-	responseSize("https://golang.org/doc")
+	pages := make(chan Page)
+	urls := []string{"https://example.com/",
+		"https://golang.org/", "https://golang.org/doc"}
+	for _, url := range urls {
+		go responseSize(url, pages)
+	}
+	for i := 0; i < len(urls); i++ {
+		page := <-pages
+		fmt.Printf("%s: %d\n", page.URL, page.Size)
+	}
 
 }
 
-func responseSize(url string) {
-	fmt.Println("getting", url)
+func responseSize(url string, channel chan Page) {
+	fmt.Println("Getting", url)
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(response.Body)
+	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(len(body))
+	channel <- Page{URL: url, Size: len(body)}
 }
